54930a014bcaccc39dfbaa2f67949ef415ebec73
Wright Nyoka <wright.nyoka.ct@gmail.com>
2020-02-12 10:36:29 +0200

    Added my functions 4,6, and 7 from the notebook to our python package, analyseSprint'

--- team2-analyse/analyseSprint.py
+++ team2-analyse/analyseSprint.py
@@ -17,10 +17,21 @@ def date_parser(list_dates):
   pass
 
 def extract_municipality_hashtags(df):
-
-  ### Code Here
-
-  pass
+  municipalityKeys = list(mun_dict.keys())
+  municipalities = []
+  hashtags = []
+  for index, row in df.iterrows():
+      municipalities.append(np.nan)
+      tweet = row['Tweets'].split()
+      tags = []
+      for word in tweet:
+          if word.startswith('#'):
+              tags.append(word.lower())
+          if municipalityKeys.count(word): municipalities[index] = mun_dict[word] 
+      hashtags.append(tags) if tags else hashtags.append(np.nan)
+  df['municipality'] = municipalities
+  df['hashtags'] = hashtags
+  return df
 
 def number_of_tweets_per_day(df):
 
@@ -28,14 +39,19 @@ def number_of_tweets_per_day(df):
 
   pass
 
-def word_spliter(df):
+def word_splitter(df):
+  tokens = []
+  for index, row in df.iterrows():
+      tokens.append(list(map(str.lower, row['Tweets'].split())))
+  df['Split Tweets'] = tokens
+  return df
 
-  ### Code Here
-
-  pass
-
-def stop_words_http_remover(df):
-
-  # Code Here
-
-  pass
+def stop_words_remover(df):
+    nstokens = []
+    for index, row in df.iterrows():
+        tempTokens = []
+        for word in list(map(str.lower, row['Tweets'].split())):
+            if word not in stop_words_dict['stopwords']: tempTokens.append(word)
+        nstokens.append(tempTokens)
+    df['Without Stop Words'] = nstokens
+    return df
