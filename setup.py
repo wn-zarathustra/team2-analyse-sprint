73f4c12889edd309be85459bcd45dd98639e65b1
A-Jay04 <60379264+A-Jay04@users.noreply.github.com>
2020-02-19 14:44:06 +0200

    Update setup.py
    version 3 after removing dashed files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='team_two_analyse',
-    version='0.2',
+    version='0.3',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A package to calculate metrics using Eskom data.',
